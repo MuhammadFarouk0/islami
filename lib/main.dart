8f5724a6cf10c9018b9cecd12daee47106234760
ahmadfarghali <ahmadfarghali@outlook.com>
2022-07-27 16:21:19 +0200

    initialize the islami prolect and empty homescreen page

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:islami/home_screen.dart';
+
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        HomeScreen.routeName:(_)=>HomeScreen(),
+      },
+      initialRoute:
+      HomeScreen.routeName,
+
+    );
+  }
+}
